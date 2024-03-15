f9e76a70e69a66ef2fe9a05631fda0b4c1d90928
SFizz405 <147613991+SFizz405@users.noreply.github.com>
2024-03-15 14:54:24 +0900

    Add files via upload

---
+++ patch.js
@@ -0,0 +1,27 @@
+(function (xhr) {
+    const open = xhr.open;
+
+    xhr.open = function (method, url, async) {
+        const send = this.send;
+
+        this.send = function (data) {
+            const rsc = this.onreadystatechange;
+
+            if (rsc) {
+                this.onreadystatechange = function () {
+                    if (this.readyState === 4 && /^https:\/\/twitter\.com\/i\/api\/graphql\//.test(this.responseURL)) {
+                        const response = JSON.parse(this.response, (key, value) => {
+                            return ["full_text", "name", "text"].includes(key) ? "🚫" : value;
+                        });
+
+                        Object.defineProperty(this, "response", { value: JSON.stringify(response) });
+                        Object.defineProperty(this, "responseText", { value: JSON.stringify(response) });
+                    };
+                    return rsc.apply(this, arguments);
+                }
+            }
+            return send.apply(this, arguments);
+        };
+        return open.apply(this, arguments);
+    };
+})(XMLHttpRequest.prototype);
